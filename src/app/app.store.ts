4efb504d9ecfe26f533d31fb84a4116a439675bb
nhaji <n.haji.tech@gmail.com>
2026-01-14 14:30:25 +0000

    Initialize api call && connect endpoint

--- src/app/app.store.ts
+++ src/app/app.store.ts
@@ -1,16 +1,18 @@
-import { inject, Injectable, signal } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { inject, Injectable, resource, signal } from '@angular/core';
+import { AppService } from './app-service';
+import { firstValueFrom } from 'rxjs';
 import { Event, NavigationEnd, Router } from '@angular/router';
-import { CHATBOT_ROUTE } from '../../features/chatbot/chatbot.routes';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { CHATBOT_ROUTE } from './features/chatbot/chatbot.routes';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AppStore {
+  private service = inject(AppService);
+  private router = inject(Router);
 
   isChatbotRoute = signal<boolean>(false);
-
-  private router = inject(Router);
 
   constructor(){
     this.router.events.pipe(takeUntilDestroyed()).subscribe((event: Event) => {
@@ -24,5 +26,8 @@ export class AppStore {
       }
     });
   }
-  
+
+  connectRessource = resource({
+    loader: () => firstValueFrom(this.service.connect()),
+  });
 }
--- src/app/core/interceptors/base-url-interceptor.ts
+++ src/app/core/interceptors/base-url-interceptor.ts
@@ -1,6 +1,8 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { LanguageService } from '../services/language.service';
+import { HttpService } from '../services/http.service';
+import { environment } from '../../../environments/environment';
 
 export const baseUrlInterceptor: HttpInterceptorFn = (req, next) => {
   if(req.url.includes('i18n')){
@@ -10,7 +12,11 @@ export const baseUrlInterceptor: HttpInterceptorFn = (req, next) => {
 
   const currentLang = languageService.getCurrentLang();
 
-  const modifiedUrl = `assets/data/${currentLang()}${req.url}`;
+  const translateRequired = req.headers.get(HttpService.TRANSLATE_REQUIRE_HEADER) === 'true';
+
+  const modifiedUrl = translateRequired 
+  ? `assets/data/${currentLang()}${req.url}`
+  : `${environment.apiUrl}${req.url}`;
 
   const modifiedReq = req.clone({
     url: modifiedUrl,
